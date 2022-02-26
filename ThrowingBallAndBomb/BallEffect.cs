007942aadd6831c154feb05d1d0f9482e2ad8120
JuHyeong Seo <sjh990721@naver.com>
2022-02-26 15:36:23 +0900

    AR 지면에 공, 폭탄 던지기 콘텐츠

---
+++ ThrowingBallAndBomb/BallEffect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallEffect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public ParticleSystem particle;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag.Equals("Player")) return;
+        Instantiate(particle, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
